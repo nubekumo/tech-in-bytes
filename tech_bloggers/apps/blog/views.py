6859ba55971e9e31d114269bac0222e9455adac4
Devel-mate <meneses.sutil@gmail.com>
2025-09-17 20:06:38 -0600

    Add upload-post functionality

--- tech_bloggers/apps/blog/views.py
+++ tech_bloggers/apps/blog/views.py
@@ -8,8 +8,8 @@ from django.urls import reverse_lazy, reverse
 from django.db.models import Q
 from django.http import JsonResponse
 from django.contrib import messages
-from .models import Post, Comment
-from taggit.models import Tag
+from .models import Post, Comment, Tag
+from .forms import PostForm
 
 class SlugRedirectMixin:
     """Mixin to handle slug redirects when slug in URL doesn't match current slug."""
@@ -64,15 +64,35 @@ class PostDetailView(SlugRedirectMixin, DetailView):
 
 class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/create_post.html'
-    fields = ['title', 'content', 'summary', 'image', 'tags', 'status']
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        return super().post(request, *args, **kwargs)
 
     def form_valid(self, form):
         form.instance.author = self.request.user
+        
+        # Check which button was clicked
+        action = self.request.POST.get('action')
+        
+        if action == 'draft':
+            form.instance.status = 'draft'
+        elif action == 'publish':
+            form.instance.status = 'published'
+            from django.utils import timezone
+            form.instance.published_at = timezone.now()
+        
         return super().form_valid(form)
 
+    def form_invalid(self, form):
+        return super().form_invalid(form)
+
     def get_success_url(self):
-        return reverse('blog:post_detail', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
+        return reverse('blog:post_manage')
 
 class PostManageView(LoginRequiredMixin, ListView):
     model = Post
@@ -85,23 +105,67 @@ class PostManageView(LoginRequiredMixin, ListView):
 
 class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, SlugRedirectMixin, UpdateView):
     model = Post
+    form_class = PostForm
     template_name = 'blog/edit_post.html'
-    fields = ['title', 'content', 'summary', 'image', 'tags', 'status']
+
+    def get_object(self, queryset=None):
+        """Override to cache the object and avoid multiple queries."""
+        if not hasattr(self, '_object'):
+            self._object = super().get_object(queryset)
+        return self._object
 
     def test_func(self):
-        post = self.get_object()
+        post = self.get_object()  # Uses cached object
         return self.request.user == post.author
 
     def get_redirect_url(self, post):
         return reverse('blog:post_edit', kwargs={'pk': post.pk, 'slug': post.slug})
 
     def get_success_url(self):
-        return reverse('blog:post_detail', kwargs={'pk': self.object.pk, 'slug': self.object.slug})
+        return reverse('blog:post_manage')
+
+    def form_valid(self, form):
+        """Override form_valid to add success message after update."""
+        messages.success(self.request, 'Your post has been updated successfully')
+        return super().form_valid(form)
+
+class PostPublishView(LoginRequiredMixin, UserPassesTestMixin, SlugRedirectMixin, UpdateView):
+    model = Post
+    template_name = 'blog/publish_post.html'
+    fields = []  # No form fields needed, we're just changing status
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+    def get_redirect_url(self, post):
+        return reverse('blog:post_publish', kwargs={'pk': post.pk, 'slug': post.slug})
+
+    def get_success_url(self):
+        return reverse('blog:post_manage')
+
+    def post(self, request, *args, **kwargs):
+        """Override post method to toggle publish status and add success message."""
+        post = self.get_object()
+        
+        # Toggle the status
+        if post.status == 'draft':
+            post.status = 'published'
+            from django.utils import timezone
+            post.published_at = timezone.now()
+            success_message = 'Your post has been published'
+        else:
+            post.status = 'draft'
+            success_message = 'Your post has been unpublished'
+        
+        post.save()
+        messages.success(request, success_message)
+        
+        return redirect(self.get_success_url())
 
 class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, SlugRedirectMixin, DeleteView):
     model = Post
     template_name = 'blog/delete_post.html'
-    success_url = reverse_lazy('blog:post_manage')
 
     def test_func(self):
         post = self.get_object()
@@ -109,6 +173,14 @@ class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, SlugRedirectMixin,
 
     def get_redirect_url(self, post):
         return reverse('blog:post_delete', kwargs={'pk': post.pk, 'slug': post.slug})
+
+    def get_success_url(self):
+        return reverse('blog:post_manage')
+
+    def post(self, request, *args, **kwargs):
+        """Override post method to add success message before deletion."""
+        messages.success(request, 'Your post has been deleted')
+        return super().post(request, *args, **kwargs)
 
 class TagListView(ListView):
     model = Tag
